dd3c83b5c9e282642b426a467eea5efbd047b9fc
jfcbxp <jfcbxp@gmail.com>
2025-06-16 23:09:01 -0300

    ✨ feat: add password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,7 +11,7 @@ export class AuthController {
 
   @Post('login')
   async login(@Body() body: AuthLoginDTO) {
-    return this.authService.login(body.id);
+    return this.authService.login(body.id, body.password);
   }
 
   @UseGuards(AuthGuard)
