225467965874a92baf859b19f70904ac32f47b2f
jfcbxp <jfcbxp@gmail.com>
2025-06-20 14:34:36 -0300

    ✨ feat: new payment types

--- src/enums/filter-types.enum.ts
+++ src/enums/filter-types.enum.ts
@@ -2,4 +2,9 @@ export enum FilterTypesEnum {
   BOL = 'Boleto',
   NP = 'Nota Promissória',
   REN = 'Renegociação',
+  DP = 'Depósito',
+  CC = 'Cartão de Crédito',
+  CD = 'Cartão de Débito',
+  PX = 'Pix',
+  CH = 'Cheque',
 }
